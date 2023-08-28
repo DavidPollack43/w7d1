15d1d623d5bca05ddf5e3b92b671324004954ec8
David <2018dpollack@gmail.com>
2023-08-28 15:42:40 -0700

    Added user controller

--- practice-for-ch-rails-ninety-nine-cats1-long-practice/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice/app/models/user.rb
@@ -11,13 +11,13 @@
 #
 class User < ApplicationRecord
     before_validation :ensure_session_token
-    validate :username, :session_token, presence: true, unique: true
-    validate :password_digest, presence: true
-    validate :password, length: {minimum: 6}, allow_nil: true
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
     attr_reader :password
 
     def password=(password)
-        self.password_digest = BCrypt::password.create(password)
+        self.password_digest = BCrypt::Password.create(password)
         @password = password
     end
     
