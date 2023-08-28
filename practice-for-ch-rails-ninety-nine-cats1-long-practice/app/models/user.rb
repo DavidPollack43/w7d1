19d0b25d70c34f930c47adb18b55bc1c423f9125
David <2018dpollack@gmail.com>
2023-08-28 14:57:48 -0700

    added all validations and password=

--- practice-for-ch-rails-ninety-nine-cats1-long-practice/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice/app/models/user.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
-    
+    before_validation :ensure_session_token
+    validate :username, :session_token, presence: true, unique: true
+    validate :password_digest, presence: true
+    validate :password, length: {minimum: 6}, allow_nil: true
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::password.create(password)
+        @password = password
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
 end
